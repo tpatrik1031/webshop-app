35a1f4002c602a27d5a6ca99377843b5338147a4
patriktoth1031 <patrik.toth@syneo.hu>
2025-02-26 12:03:24 +0100

    docker init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        port: 5173,
+        host: '0.0.0.0',
+        hmr: {
+            clientPort: 5173,
+            host: '127.0.0.1'
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
